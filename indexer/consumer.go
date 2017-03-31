356c299c5b03ebd4ad7bb8cbfe81ebc37ccf996d
Stefan Prodan <stefan.prodan@gmail.com>
2017-04-01 00:23:54 +0300

    undo dedicated rdb session per collector

--- indexer/consumer.go
+++ indexer/consumer.go
@@ -29,43 +29,32 @@ func (c *Consumer) Consume() {
 
 func (c *Consumer) DockerConsume() {
 	c.NatsConnection.QueueSubscribe("docker", c.Config.CollectorQueue, func(m *nats.Msg) {
-		crepo, err := NewRepository(c.Config)
-		if err != nil {
-			log.Fatalf("RethinkDB connection error %v", err)
-		}
 		var payload models.DockerPayload
-		err = json.Unmarshal(m.Data, &payload)
+		err := json.Unmarshal(m.Data, &payload)
 		if err != nil {
 			log.Errorf("Docker payload unmarshal error %v", err)
 		} else {
 			log.Debugf("Docker payload received from host %v running containes %v", payload.Host.Name, payload.Host.ContainersRunning)
-			crepo.HostUpsert(payload.Host)
+			c.Repository.HostUpsert(payload.Host)
 
 			for _, container := range payload.Containers {
-				crepo.ContainerUpsert(container)
+				c.Repository.ContainerUpsert(container)
 			}
-			crepo.Session.Close()
 		}
 	})
 }
 
 func (c *Consumer) ConsulConsume() {
 	c.NatsConnection.QueueSubscribe("consul", c.Config.CollectorQueue, func(m *nats.Msg) {
-		crepo, err := NewRepository(c.Config)
-		if err != nil {
-			log.Fatalf("RethinkDB connection error %v", err)
-		}
-
 		var payload models.ConsulPayload
-		err = json.Unmarshal(m.Data, &payload)
+		err := json.Unmarshal(m.Data, &payload)
 		if err != nil {
 			log.Errorf("Consul payload unmarshal error %v", err)
 		} else {
 			log.Debugf("Consul payload received %v checks", len(payload.HealthChecks))
 			for _, check := range payload.HealthChecks {
-				crepo.CheckUpsert(check)
+				c.Repository.CheckUpsert(check)
 			}
-			crepo.Session.Close()
 		}
 	})
 }
