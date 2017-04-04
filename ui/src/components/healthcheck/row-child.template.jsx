55456018f7f94717c3e3c06118c526f0a1ff1ce8
Stefan Prodan <stefan.prodan@gmail.com>
2017-04-04 20:16:31 +0300

    set default check output

--- ui/src/components/healthcheck/row-child.template.jsx
+++ ui/src/components/healthcheck/row-child.template.jsx
@@ -1,4 +1,7 @@
 export default function(h, row) {
+    if (row.output.length < 1){
+        row.output = 'TIMEOUT'
+    }
    return (
     <div class="row">
         <p>Check ID: {row.service_id}</p>
--- ui/src/components/healthchecks/row-child.template.jsx
+++ ui/src/components/healthchecks/row-child.template.jsx
@@ -1,4 +1,7 @@
 export default function(h, row) {
+    if (row.output.length < 1){
+        row.output = 'TIMEOUT'
+    }
    return (
     <div class="row">
         <p>Consul Service ID: {row.service_id}</p>
