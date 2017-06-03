35215cda6d9f9f26267ab78e9c265ddbd5743968
Stefan Prodan <stefan.prodan@gmail.com>
2017-06-03 12:21:09 +0300

    yaml collector config

--- agent/config.go
+++ agent/config.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
+)
+
 // Config holds global configuration, defaults are provided in main.
 // Agent config is populated from startup flag.
 type Config struct {
@@ -14,4 +24,47 @@ type Config struct {
 	VSphereExclude         string `m:"VSphereExclude"`
 	VSphereCollectInterval int    `m:"VSphereCollectInterval"`
 	Nats                   string `m:"Nats"`
+}
+
+type CollectorConfig struct {
+	Docker  ApiCollectorConfig `json:"docker" yaml:"docker"`
+	Consul  ApiCollectorConfig `json:"consul" yaml:"consul"`
+	VSphere ApiCollectorConfig `json:"vsphere" yaml:"vsphere"`
+}
+
+type ApiCollectorConfig struct {
+	Endpoints []string `json:"endpoints" yaml:"endpoints"`
+	Include   []string `json:"include" yaml:"include"`
+	Exclude   []string `json:"exclude" yaml:"exclude"`
+	Cron      string   `json:"cron" yaml:"cron"`
+}
+
+func LoadCollectorConfig(dir string, name string) (*CollectorConfig, error) {
+	cfg := &CollectorConfig{}
+	cfgPath := ""
+	err := filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+		if strings.Contains(path, name+".yml") || strings.Contains(path, name+".yaml") {
+			cfgPath = path
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, errors.Wrapf(err, "Reading from %v failed", dir)
+	}
+
+	if len(cfgPath) < 1 {
+		return nil, errors.Errorf("Collector config %v not found", name)
+	}
+
+	data, err := ioutil.ReadFile(cfgPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Reading %v failed", cfgPath)
+	}
+
+	if err := yaml.Unmarshal(data, cfg); err != nil {
+		return nil, errors.Wrapf(err, "Parsing %v failed", cfgPath)
+	}
+
+	return cfg, nil
 }
