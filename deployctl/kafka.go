6bde21d9de967bdb8845e51742d36bf331fed1bf
Stefan Prodan <stefan.prodan@gmail.com>
2017-07-18 11:50:27 +0300

    exec kafka topic with retention and partitions

--- deployctl/kafka.go
+++ deployctl/kafka.go
@@ -29,7 +29,7 @@ func (m KafkaDeploy) Migrate() error {
 	log.Printf("Loading topics from %s", m.Dir)
 	err := filepath.Walk(m.Dir, func(path string, f os.FileInfo, err error) error {
 		if strings.Contains(path, ".topics") {
-			list, err := readTopics(path)
+			list, err := readTopicsCommand(path)
 			if err != nil {
 				return err
 			}
@@ -40,9 +40,9 @@ func (m KafkaDeploy) Migrate() error {
 	if err != nil {
 		return errors.Wrapf(err, "loading topics from %s failed", m.Dir)
 	}
-	migration = stringsDistinct(migration)
 	sort.Strings(migration)
 	//log.Print(migration)
+
 	log.Printf("Topics found %d in migration", len(migration))
 
 	log.Printf("Loading topics via SSH from %s", m.HostTo)
@@ -56,7 +56,6 @@ func (m KafkaDeploy) Migrate() error {
 		if len(line) > 2 {
 			existingTopics = append(existingTopics, strings.TrimSpace(line))
 		}
-
 	}
 	log.Printf("Topics found %d on %s", len(existingTopics), m.HostTo)
 	//log.Print(existingTopics)
@@ -65,7 +64,9 @@ func (m KafkaDeploy) Migrate() error {
 	for _, mi := range migration {
 		found := false
 		for _, mr := range existingTopics {
-			if strings.TrimSpace(mr) == strings.TrimSpace(mi) {
+			// strip topic specs as retention or partitions
+			topicName := strings.Split(mi, " ")[0]
+			if strings.TrimSpace(mr) == strings.TrimSpace(topicName) {
 				found = true
 				//log.Printf("Topic found %v", mi)
 				break
@@ -78,12 +79,16 @@ func (m KafkaDeploy) Migrate() error {
 
 	if len(newTopics) > 0 {
 		log.Printf("New topics found %v", newTopics)
-		for _, metric := range newTopics {
-			log.Printf("Inserting new topic %s", metric)
-			cmd := fmt.Sprintf("/usr/bin/kafka-topics --zookeeper 127.0.0.1:2181 --create --topic %s --replication-factor 3", metric)
+		for _, topicCommand := range newTopics {
+			log.Printf("Inserting new topic %s", topicCommand)
+			cmd := fmt.Sprintf("/usr/bin/kafka-topics --zookeeper 127.0.0.1:2181 --create --topic %s --replication-factor 3 --partitions 1", topicCommand)
+			if strings.Contains(topicCommand, "partitions") {
+				cmd = fmt.Sprintf("/usr/bin/kafka-topics --zookeeper 127.0.0.1:2181 --create --topic %s --replication-factor 3", topicCommand)
+			}
+			log.Print(cmd)
 			_, err := m.Ssh.RunCommand(cmd)
 			if err != nil {
-				return errors.Wrapf(err, "kafka-topics create %s failed", metric)
+				return errors.Wrapf(err, "kafka-topics create %s failed", topicCommand)
 			}
 		}
 	} else {
@@ -93,7 +98,7 @@ func (m KafkaDeploy) Migrate() error {
 	return nil
 }
 
-func readTopics(path string) ([]string, error) {
+func readTopicsCommand(path string) ([]string, error) {
 	topics := make([]string, 0)
 
 	if file, err := os.Open(path); err == nil {
@@ -101,8 +106,7 @@ func readTopics(path string) ([]string, error) {
 
 		scanner := bufio.NewScanner(file)
 		for scanner.Scan() {
-			// strip topic specs as retention or partitions
-			val := strings.Split(scanner.Text(), " ")[0]
+			val := scanner.Text()
 			if len(val) > 2 {
 				topics = append(topics, val)
 			}
